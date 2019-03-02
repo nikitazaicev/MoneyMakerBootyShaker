4605c2cd01648635558e5cff7e3aa872f8235e5f
mitja <mitja@DESKTOP-KEFAR2A>
2019-03-02 17:20:56 +0100

    sql

--- EXPO/testInnput.sql
+++ EXPO/testInnput.sql
@@ -1,10 +1,10 @@
 set search_path = EXPO;
 
 	
-INSERT INTO EXPO.deltager VALUES
+INSERT INTO EXPO.Stand VALUES
 	('11111111', 'Nakita2', 'Zaicev2', 'pass2', 'OTHER');	
 
-INSERT INTO EXPO.deltagerliste VALUES
+INSERT INTO EXPO.StandHistory VALUES
 	('listenavn1','11111111'),
 	('listenavn2','11111112');
  
