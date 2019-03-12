90a65235d65223df6c759602ff954f3bd488a06c
Julian <31210089+H183549@users.noreply.github.com>
2019-03-12 12:18:00 +0100

    Julius har gjort ting v2

---
+++ EXPO/src/servlets/JuryServlet.java
@@ -0,0 +1,40 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import objects.TimeStatsEAO;
+
+/**
+ * Servlet implementation class JuryServlet
+ */
+@WebServlet("/JuryServlet")
+public class JuryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    @EJB
+    TimeStatsEAO eao;
+    
+    	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+}
