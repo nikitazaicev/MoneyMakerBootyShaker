f8cf4a7fc881bf2755b9007433c7c040e7c5a545
Petter <pette@158.37.229.146>
2019-03-12 12:31:45 +0100

    Stand fix?

--- EXPO/src/objects/Stand.java
+++ EXPO/src/objects/Stand.java
@@ -22,18 +22,37 @@ public class Stand implements Serializable{
 	private String id;
 	
 	private int score;
+	private int antStemmer;
 	
 	@OneToMany(mappedBy="stand")
 	private Set<TimeStats> historikk;
 	
 	public Stand() {
 		score = 0;
+		setAntStemmer(0);
 	}
 	
-	public Stand(int score) {
+	public Stand(int score, int antStemmer) {
 		super();
 		this.score = score;
+		this.setAntStemmer(antStemmer);
 		historikk = new HashSet<TimeStats>();
+	}
+	
+	public void vote(int vote) {
+		// Hver bruker skal kunne sende inn en vote
+		// Denne voten skal lagres
+		score = score + vote;
+		setAntStemmer(getAntStemmer() + 1);
+	}
+	public void reVote(int gammel, int nyVote) {
+		// Hente inn gammel vote fra session
+		// Fjern gammel vote fra score og legg til nyVote
+		// antStemmer forblir dem samme 
+		score = score - gammel + nyVote;
+	}
+	public int averageVote() {
+		return score / antStemmer;
 	}
 	
 	public String getId() {
@@ -51,6 +70,13 @@ public class Stand implements Serializable{
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
+
+	public int getAntStemmer() {
+		return antStemmer;
+	}
+
+	public void setAntStemmer(int antStemmer) {
+		this.antStemmer = antStemmer;
+	}
 	
 }
